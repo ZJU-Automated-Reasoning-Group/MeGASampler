bbf1ae5bf0761fc0d10500eed6fd49db2d85184c
Matan Peled <chaosite@gmail.com>
2022-05-18 13:19:46 +0300

    scripts: Fix select_benchmarks script

--- scripts/select_benchmarks.sh
+++ scripts/select_benchmarks.sh
@@ -7,12 +7,13 @@ THIS_TALL=15
 
 [ -z "${INPUT}" ] || [ -z "${OUTPUT}" ] && { echo "$0 INPUT_DIR OUTPUT_DIR"; exit 1; }
 
-find ${INPUT} -maxdepth 1 -type d -and -not -iname '.' | while read inputdir; do
-	dir="$(realpath --relative-to="${INPUT}" "${inputdir}")"
-	benchmarks=("${INPUT}/${dir}"/**/*.smt2)
-	echo ${dir}: ${#benchmarks[@]}
-	if [ ${#benchmarks[@]} -lt ${THIS_TALL} ]; then continue; fi
-	#echo "${dir}: More than ${THIS_TALL} benchmarks, picking some"
-	#echo mkdir -p "${OUTPUT}/${dir}"
-	#find "${INPUT}/${dir}" -name '*.smt2' -maxdepth 1 -print0 | shuf -z -n ${THIS_TALL} | xargs -0 echo cp -at "${OUTPUT}/${dir}"
+find ${INPUT} -type f -iname '*.smt2' -exec dirname -z "{}" \; | \
+    sort -zu | while read inputdir; do
+    dir="$(realpath --relative-to="${INPUT}" "${inputdir}")"
+    benchmarks=("${INPUT}/${dir}"/**/*.smt2)
+    echo ${dir}: ${#benchmarks[@]}
+    if [ ${#benchmarks[@]} -lt ${THIS_TALL} ]; then continue; fi
+    echo "${dir}: More than ${THIS_TALL} benchmarks, picking some"
+    echo mkdir -p "${OUTPUT}/${dir}"
+    find "${INPUT}/${dir}" -name '*.smt2' -maxdepth 1 -print0 | shuf -z -n ${THIS_TALL} | xargs -0 echo cp -at "${OUTPUT}/${dir}"
 done
